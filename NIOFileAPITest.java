80776a420f50684596ecf58b2e717c1489ee9c7f
unknown <nituldeori63@gmail.com>
2020-12-09 20:23:54 +0530

    Added: Program to demonstrate File operations like check file exist, delete file etc

---
+++ NIOFileAPITest.java
@@ -0,0 +1,57 @@
+package com.employeepayrollservice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import static java.awt.Event.HOME;
+
+public class NIOFileAPITest {
+    private static String HOME = System.getProperty("user.home");
+    private static String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
+        Path homePath = Paths.get(HOME);
+        Assert.assertTrue(Files.exists(homePath));
+
+        Path playPath = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
+        Assert.assertTrue(Files.notExists(playPath));
+
+        Files.createDirectory(playPath);
+        Assert.assertTrue(Files.exists(playPath));
+
+        IntStream.range(1, 10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
+            Assert.assertTrue(Files.notExists(tempFile));
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
+
+            }
+            Assert.assertTrue(Files.exists(tempFile));
+        });
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() &&
+                path.toString().startsWith("temp")).forEach(System.out::println);
+
+    }
+
+    @Test
+    public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException{
+        Path dir= Paths.get(HOME+"/"+PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExamle(dir).processEvents();
+    }
+
+
+}
+
